a980aba30a360924bbb33b006f976d7485de1d5e
Achim Fritz <af@achimfritz.de>
2026-02-13 07:37:30 +0100

    [TASK] rm pages tca

--- Configuration/TCA/Overrides/pages.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$GLOBALS['TCA']['pages']['ctrl']['typeicon_classes']['contains-formlogs'] = 'formhandler-foldericon';
-
-$GLOBALS['TCA']['pages']['columns']['module']['config']['items'][] = [
-    'LLL:EXT:formhandler/Resources/Private/Language/locallang.xlf:title',
-    'formlogs',
-    'EXT:formhandler/Resources/Public/Icons/Extension.gif',
-];
